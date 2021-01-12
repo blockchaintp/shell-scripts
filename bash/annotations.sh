699b41521431d9ebb8503d0f2fd3feacefffc8d6
Kevin O'Donnell <kevin@blockchaintp.com>
2021-01-12 02:09:03 -0500

    fix: correct an implicit recursion in annotations
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- bash/annotations.sh
+++ bash/annotations.sh
@@ -2,6 +2,7 @@
 
 # shellcheck source=doc.sh
 source "$(dirname "${BASH_SOURCE[0]}")/doc.sh"
+
 # shellcheck source=bash-logger.sh
 source "$(dirname "${BASH_SOURCE[0]}")/bash-logger.sh"
 @package .
@@ -9,6 +10,6 @@ source "$(dirname "${BASH_SOURCE[0]}")/bash-logger.sh"
 function deprecated() {
   @doc Mark a function as deprecateds
   local newfunc=$1
-  DEBUG "${FUNCNAME[1]} is deprecated. Replace with $newfunc"
+  log::debug "${FUNCNAME[1]} is deprecated. Replace with $newfunc"
   "$@"
 }
