612a1011208b7ed8709dd02fbdb2dbc6bd28a348
Kevin O'Donnell <kevin@blockchaintp.com>
2021-01-18 00:59:06 -0500

    fix: correct annotation declaration and bashadoc
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- bash/annotations.sh
+++ bash/annotations.sh
@@ -5,11 +5,14 @@ source "$(dirname "${BASH_SOURCE[0]}")/includer.sh"
 @include doc
 @include log
 
-@package .
+@package annotations
 
-function deprecated() {
+function annotations::deprecated() {
   @doc Mark a function as deprecated
   local newfunc=$1
   log::debug "${FUNCNAME[1]} is deprecated. Replace with $newfunc"
   "$@"
+}
+function deprecated() {
+  annotations::deprecated "$@"
 }
